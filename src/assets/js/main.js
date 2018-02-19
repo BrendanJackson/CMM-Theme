22d7ca66a8be6f7e05409f90bd26954360ce0735
brendan <brendan@mediocrecreative.com>
2018-02-19 13:10:55 -0500

    checkpoint: Calendar styles

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   setTimeout(function(){
 
     $(".fc-month-button").removeClass("fc-state-active");
-    $(".fc-agendaWeek-button").addClass("fc-state-active");
+    $(".fc-basicWeek-button").addClass("fc-state-active");
 
   }, 500);
 });
